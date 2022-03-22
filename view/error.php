30b874609b1f1bc13fd041826c0ade68bfa60c4c
aymane <aymanedarj7@gmail.com>
2022-03-22 12:32:19 +0100

    create the error404 page an include the head and the navabar and link the js file

---
+++ view/error.php
@@ -0,0 +1,16 @@
+<?php
+$path = ".";
+$display ="d-block";
+include '../includes/head.php';
+include '../includes/nav-bar.php';
+?>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+</body>
+
+</html>
+
+
