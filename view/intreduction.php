cc013a74247d6d57c4e1e9671f2751bb62cdb902
khalid hagane <khalidhagane62@gmail.com>
2022-03-23 00:23:40 +0100

    ajoute page intreduction

---
+++ view/intreduction.php
@@ -0,0 +1,44 @@
+<?php
+$path = ".";
+$display ="d-block";
+include '../includes/head.php';
+include '../includes/nav-bar.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="/style.css">
+    <title>intreduction</title>
+</head>
+<body style="height:100vh;">
+    <main class=" d-flex flex-column justify-content-center align-items-center gap-4" style="height:80vh;">
+        <div class="card p-3 px-5 " style="width: 70%; background-color:#96C5DC;">
+            
+                <h1 class="card-title text-center " style="color:#1078AD;" >تمهيد</h1>
+                
+                <p class="card-text text-center">هاد التطبيق
+                    مجاني، درناه باش نسهلو على الناس لي عندها إصابة
+                    بڤيروس كورونا19 يعرفو الحالة ديالهم و آش خصهم يديرو
+                    . منقدوش نضمنو بنسبة 100% دقة المعلومات لي تيعطيها
+                    هاد التطبيق. يقر المستخدم بأن التطبيق 
+                    ، بما في ذلك الاختبار والمعلومات الأخرى لي فيه 
+                    ، لا تشكل توصية او فحص او وصفة طبية او اي حاجة عندها طبيعة طبية. 
+                </p>
+                <div class=" d-flex justify-content-center "  ><hr class="bg-warning" style="width:12rem; height:3px"></div>
+                <p class=" text-center">هاد التطبيق متيعوضش الطبيب ديالك او الصيدلي لي يقدرو يعطيوك نصيحة أفضل. هادشي علاش قبل ما دير والو استاشر مع شي طبيب.
+
+                </p>
+                <p class=" text-center">المعلومات ديال هاد التطبيق هي المستوى الاول باش نقدو نحددو الحالات الممكنة. </p>
+            
+        </div>
+        <button class="ques-btn" style="width:15rem;">نبداو الاختبار</button>
+    </main>
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
