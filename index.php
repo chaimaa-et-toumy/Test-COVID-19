7d401812f7c50a79405b83c1d729fab37168483a
aymane <aymanedarj7@gmail.com>
2022-03-22 10:02:44 +0100

    Include the head in the index page and change the path of the included files

--- index.php
+++ index.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <link rel="stylesheet" href="style.css">
-    <title>test covid</title>
-</head>
-
-<body>
-    <?php
+<?php
+    $path = "";
     $display = "d-none";
-    include 'view/nav-bar.php';
-    ?>
+    include './includes/head.php';
+    include './includes/nav-bar.php';
+?>
     <div class="text-center header2 p-3">
         <h3>LA DERNIÈRE INFO MÉDICALE </h3>
         <p>La prise d'anti-inflammatoires (ibuprofène, cortisone ...) pourrait être un facteur d'aggravation de
