7a407bf5b89614c17479d53e5ee1094faa2ec7d5
khalid hagane <khalidhagane62@gmail.com>
2022-03-23 10:25:51 +0100

    conflit

--- index.php
+++ index.php
@@ -6,28 +6,37 @@ include './includes/nav-bar.php';
 ?>
 <div class="text-center header2 p-1">
     <h3>LA DERNIÈRE INFO MÉDICALE </h3>
-    <p class="m-0">La prise d'anti-inflammatoires (ibuprofène, cortisone ...) pourrait être un facteur d'aggravation de
-        l’infection. En cas de fièvre, prenez du paracétamol.
+    <p class="m-1 para_1">
+        La prise d'anti-inflammatoires (ibuprofène, cortisone ...) pourrait être un facteur <br>
+        d'aggravation de
+        l’infection. En cas de fièvre, prenez du paracétamol. <br>
         N'arrêtez pas votre traitement ! Consultez votre médecin ou votre pharmacien.
     </p>
 </div>
-<div class="d-flex justify-content-evenly flex-wrap-reverse flex-lg-nowrap text-center p-2">
+<div class="d-flex justify-content-around flex-wrap flex-lg-nowrap text-center p-2">
 
     <div class="partie_1">
-        <h3 class="fw-bold">Vous pensez avoir été exposé au Coronavirus COVID-19 et vous avez des symptômes ?</h3>
-        <img src="img/SVG/1f637.svg" alt="icon">
-        <p class="text_accueille">
+        <img src="img/SVG/Groupe76.png" alt="background" height="398" class="w-100">
+    </div>
+
+    <div class="partie_2 col-lg-5 col-12">
+
+        <h4 class="fw-bold pt-3">
+            Vous pensez avoir été exposé au Coronavirus COVID-19 et vous avez des symptômes ?
+        </h4>
+
+        <img src="img/SVG/1f637.svg" alt="icon" class="pt-2">
+
+        <p class="text_accueille h4 pt-2 pb-2">
             Je reste vigilant ? <br>
             Je programme une téléconsultation ? <br>
             J'appelle le 141 ? <br>
             Faites le test pour répondre en citoyen éclairé selon vos symptômes.
         </p>
-        <button class="ques-btn btn_test w-100"> Démarrer le test </button>
 
-    </div>
+        <a href="view/introduction" class="btn ques-btn btn_test w-100 fw-bold fs-5 mb-sm-4 mb-lg-0"> Démarrer le test
+        </a>
 
-    <div class="partie_2">
-        <img src="img/SVG/Groupe76.png" width="577" height="394" alt="background">
     </div>
 
 </div>
--- view/error.php
+++ view/error.php
@@ -7,16 +7,16 @@ include '../includes/nav-bar.php';
 <div class=" d-flex gap-3 justify-content-center flex-column text-center">
     <img alt="#" class="col-7 mx-auto mt-5" src="../img/SVG/Group137.svg">
     <h3 class="fw-bolder h2">Tu es perdu</h3>
-    <button class="mt-3 ques-btn">Retourner à la page d'acceuil</button>
+    <a href="../index.php"><button class="mt-3 ques-btn col-9 col-md-3 fw-bolder h5">Retourner à la page d'acceuil</button></a>
 </div>
-<div class="text-center mt-5">
+<div class="text-center my-5">
     <img alt="logo" class="m-auto" src="../img/SVG/logo.svg">
-    <div class=" d-flex flex-column flex-md-row  justify-content-center mt-5">
+    <div class=" d-flex flex-column flex-md-row  justify-content-center  gap-5 mt-5">
         <div class="col">
-            <ul class="d-flex flex-column list-unstyled">
-                <li><a href="#" class="link-language btn mt-3">عربي <i class="fas fa-globe" style="color : #F4CF1B"></i> </a></li>
-                <li><a href="#" class=" btn  fw-bold mt-4"> Accueil </a></li>
-                <li><a href="#" class=" btn fw-bold blue mt-4 "> Conseils </a></li>
+            <ul class="d-flex flex-row m-auto justify-content-center flex-md-column  gap-3 m-0 list-unstyled">
+                <li><a href="#" class="link-language btn">عربي <i class="fas fa-globe" style="color : #F4CF1B"></i> </a></li>
+                <li><a href="#" class=" btn p-0 fw-bold "> Accueil </a></li>
+                <li><a href="#" class=" btn p-0 fw-bold blue "> Conseils </a></li>
             </ul>
         </div>
         <div class="col d-flex flex-column gap-3">
@@ -27,7 +27,7 @@ include '../includes/nav-bar.php';
             <span class="fw-bold green1 h5 m-0">114</span>
         </div>
         <div class="col">
-            <p class="text-center m-auto h6 text-secondary col-8">
+            <p class="mx-auto h6 text-secondary col-10">
                 Algorithme d'orientation mis à jour sur les recommandations en vigueur
                 au 22/03/2020 à 16h15.  Ce site d’information n’est pas un dispositif médical,
                 il ne délivre pas d’avis médical. Projet solidaire d'urgence,financé par l’Alliance
