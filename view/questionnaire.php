9a82c2461b94c96d49a0d6f76c42e3580f656c58
Abdessalam-Elboukri <abdessalamelboukri00@gmail.com>
2022-03-27 10:40:20 -0700

    add validation to all the inputs also i included the introduction page into questionnaire

--- view/questionnaire.php
+++ view/questionnaire.php
@@ -17,9 +17,6 @@ include '../includes/nav-bar.php';
     margin-bottom: 35px;
 }
 
-.carousel-item {
-    height: 150px;
-}
 
 .bg-progress {
     background-color: var(--blue1) !important;
@@ -33,13 +30,13 @@ include '../includes/nav-bar.php';
 <div class="d-flex justify-content-center w-100 mt-4">
     <div class="col-md-4 col-sm-7 col-9">
         <div class="ques-line">
-            <span><i></i>
+            <span><i class="stepper-blue" id="stepper_info"></i>
                 <h5>Information</h5>
             </span>
-            <span><i></i>
+            <span><i class="test" id="stepper_ques"></i>
                 <h5>Questionnaire</h5>
             </span>
-            <span><i></i>
+            <span><i id="stepper_reslt"></i>
                 <h5>Resulte</h5>
             </span>
         </div>
@@ -47,7 +44,7 @@ include '../includes/nav-bar.php';
 </div>
 
 <!-- Progress bar -->
-<div class="d-flex justify-content-center mt-5">
+<div class="d-none justify-content-center mt-5" id="progress_bar">
     <div class="progress  col-md-5 col-sm-7 col-10 mt-1">
         <div class="progress-bar bg-progress" style="width:4.55%;">
         </div>
@@ -56,21 +53,43 @@ include '../includes/nav-bar.php';
 
 </div>
 <div class="mt-5">
-    <div id="carouselExampleControls" class="carousel slide d-flex flex-column p-4" style="border: 1px solid #1078AD;"
+    <div id="carouselExampleControls" class="carousel slide d-flex flex-column p-4"
         data-bs-interval="false">
-        <form action="" name="symbtome">
+
             <div class="carousel-inner text-center  px-sm-5">
-                <div class="carousel-item active question">
+            <!--  -->
+            <div class="carousel-item active question">
+                <main class=" introduction d-flex flex-column justify-content-center align-items-center gap-4">
+                    <div class="content p-3 px-5" style="width: 80%;">
+                        <h1 class="card-title text-center " >Préambule</h1>
+                        <p class="card-text text-center m-0">L’application est fournie à titre gratuit, en l’état,
+                            uniquement à des fins d’information pour contribuer à fluidifier la prise en
+                            charge des personnes par les services d’urgences pendant l’épidémie de Coronavirus
+                            COVID-19. L’exhaustivité, l’exactitude, le caractère à jour des informations et
+                            contenus mis à disposition dans cette application, ou leur adéquation à des 
+                            finalités particulières, ne sont pas garantis. 
+                        </p>
+                        <div class=" d-flex justify-content-center "><hr class="bg-warning opacity-75" style="width:12rem; height:3px"></div>
+                        <p class=" text-center"> Cette application ne substitue pas votre médecin qui est plus apte à vous donner plus de précision.</p>               
+                        <p class=" text-center">Les informations de cette application est le premier niveau de la détection des cas possibles du COVID-19. </p>
+                    </div>
+                    <button id="demarrer" class="ques-btn fw-bold precedent text-white text-decoration-none px-4 py-2 mt-4 h4" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="next" >
+                        Démarrer le test
+                    </button>
+                </main>     
+            </div>
+                <div class="carousel-item question">
                     <p class="h3 q1 question-color">Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours
                         (frissons, sueurs) ?
                     </p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio1"
                             value="option1">
                         <label class="form-check-label value" for="inlineRadio1">Oui</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio2"
                             value="option2">
                         <label class="form-check-label value" for="inlineRadio2">Non</label>
                     </div>
@@ -79,7 +98,7 @@ include '../includes/nav-bar.php';
                     <p class="h3 q2 question-color">Quelle est votre température corporelle ? </p>
                     <div class="d-flex justify-content-center">
                         <div class="input-group w-50">
-                            <input type="number" class="form-control" placeholder="34-42" />
+                            <input type="number" class="form-control inputs" id="input_temperateur" placeholder="34-42" />
                             <span class="btn btn-light" style="border: 1px solid #ced4da;">degre</span>
                         </div>
                     </div>
@@ -89,12 +108,12 @@ include '../includes/nav-bar.php';
                         Ces derniers jours , avez-vous une toux ou une augmentation de votre toux habituelle ?
                     </p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio3"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio3"
                             value="option1">
                         <label class="form-check-label value" for="inlineRadio3">Oui</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio4"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio4"
                             value="option2">
                         <label class="form-check-label value" for="inlineRadio4">Non</label>
                     </div>
@@ -104,12 +123,12 @@ include '../includes/nav-bar.php';
                         jours ?
                     </p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio5"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio5"
                             value="option1">
                         <label class="form-check-label value" for="inlineRadio5">Oui</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio6"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio6"
                             value="option2">
                         <label class="form-check-label value" for="inlineRadio6">Non</label>
                     </div>
@@ -118,12 +137,12 @@ include '../includes/nav-bar.php';
                     <p class="h3 q5 question-color">Ces derniers jours, avez-vous un mal de gorge ?
                     </p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio7"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio7"
                             value="option1">
                         <label class="form-check-label value" for="inlineRadio7">Oui</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio8"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio8"
                             value="option2">
                         <label class="form-check-label value" for="inlineRadio8">Non</label>
                     </div>
@@ -133,12 +152,12 @@ include '../includes/nav-bar.php';
                         selles molles.
                     </p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio9"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio9"
                             value="option1">
                         <label class="form-check-label value" for="inlineRadio9">Oui</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio10"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio10"
                             value="option2">
                         <label class="form-check-label value" for="inlineRadio10">Non</label>
                     </div>
@@ -149,12 +168,12 @@ include '../includes/nav-bar.php';
                         à vous reposer plus de la moitié de la journée ?
                     </p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio11"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio11"
                             value="option1">
                         <label class="form-check-label value" for="inlineRadio11">Oui</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio12"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio12"
                             value="option2">
                         <label class="form-check-label value" for="inlineRadio12">Non</label>
                     </div>
@@ -164,12 +183,12 @@ include '../includes/nav-bar.php';
                         depuis plus de 24h ?
                     </p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio13"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio13"
                             value="option1">
                         <label class="form-check-label value" for="inlineRadio13">Oui</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio14"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio14"
                             value="option2">
                         <label class="form-check-label value" for="inlineRadio14">Non</label>
                     </div>
@@ -179,12 +198,12 @@ include '../includes/nav-bar.php';
                         inhabituel lorsque vous parlez ou faites un petit effort ?
                     </p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio15"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio15"
                             value="option1">
                         <label class="form-check-label value" for="inlineRadio15">Oui</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio16"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio16"
                             value="option2">
                         <label class="form-check-label value" for="inlineRadio16">Non</label>
                     </div>
@@ -193,22 +212,22 @@ include '../includes/nav-bar.php';
                     <p class="h3 q5 question-color">Actuellement, comment vous vous sentez ?
                     </p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio18"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio18"
                             value="option1">
                         <label class="form-check-label" for="inlineRadio18">Bien</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5 " type="radio" name="inlineRadioOptions" id="inlineRadio19"
+                        <input class="form-check-input inputs fs-5 " type="radio" name="inlineRadioOptions" id="inlineRadio19"
                             value="option2">
                         <label class="form-check-label" for="inlineRadio19">Assez bien</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio20"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio20"
                             value="option2">
                         <label class="form-check-label" for="inlineRadio20">Fatigue(e)</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio21"
+                        <input class="form-check-input inputs fs-5" type="radio" name="inlineRadioOptions" id="inlineRadio21"
                             value="option2">
                         <label class="form-check-label" for="inlineRadio21"> Tres Fatigue(e)</label>
                     </div>
@@ -218,7 +237,7 @@ include '../includes/nav-bar.php';
                         spécifique.</p>
                     <div class="d-flex justify-content-center">
                         <div class="input-group w-50">
-                            <input type="number" class="form-control" placeholder="15-110" />
+                            <input type="number" class="form-control inputs" id="input_age"  placeholder="15-110" />
                             <span" class="btn btn-light" style="border: 1px solid #ced4da;"> Age </span>
                         </div>
                     </div>
@@ -228,7 +247,7 @@ include '../includes/nav-bar.php';
                         qui est un facteur influençant le risque de complications de l’infection.</p>
                     <div class="d-flex justify-content-center">
                         <div class="input-group w-50">
-                            <input type="number" class="form-control" placeholder="26" />
+                            <input type="number" class="form-control inputs" id="input_poid" placeholder="26" />
                             <span" class="btn btn-light" style="border: 1px solid #ced4da;"> Kg </span>
                         </div>
                     </div>
@@ -239,7 +258,7 @@ include '../includes/nav-bar.php';
                         corporelle qui est un facteur influençant le risque de complications de l’infection.</p>
                     <div class="d-flex justify-content-center">
                         <div class="input-group w-50">
-                            <input type="number" class="form-control" placeholder="80-250" />
+                            <input type="number" class="form-control inputs" id="input_tall" placeholder="80-250" />
                             <span" class="btn btn-light" style="border: 1px solid #ced4da;"> cm </span>
                         </div>
                     </div>
@@ -247,13 +266,13 @@ include '../includes/nav-bar.php';
             </div>
         </form>
 
-        <div class="buttons d-flex justify-content-center gap-5 mt-5">
+        <div class="buttons d-none justify-content-center gap-5 mt-5" id="buttons-np">
             <button class="btn fw-bold precedent text-white px-4 py-2 btn-prv" data-bs-target="#carouselExampleControls"
-                data-bs-slide="prev">
+                data-bs-slide="prev" id="prev_ques">
                 précedent
             </button>
             <button class="btn fw-bold suivant text-white px-4 py-2 btn-next" data-bs-target="#carouselExampleControls"
-                data-bs-slide="next">
+                data-bs-slide="next" id="next_ques">
                 suivant
             </button>
             <a href="Resulte.php" class="btn fw-bold suivant text-white px-4 py-2 btn-termine d-none">
